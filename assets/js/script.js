46bb59312306455a14a238f7c4cd72395b997176
williamtyn <william@tyner.se>
2022-09-22 15:44:17 +0000

    remove unused variables and add semicolon in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,81 @@
 // Global variables
-const yourChoice = document.getElementById("your-choice")
-const computerChoice = document.getElementById("computer-choice")
-const result = document.getElementById("result")
-const rockChoice = document.getElementById("rock")
-const paperChoice = document.getElementById("paper")
-const scissorChoice = document.getElementById("scissor")
-const allChoices = document.getElementsByTagName("button")
-const computerScore = document.getElementById("computer-score")
+const yourChoice = document.getElementById("your-choice");
+const computerChoice = document.getElementById("computer-choice");
+const result = document.getElementById("result");
+const rockChoice = document.getElementById("rock");
+const paperChoice = document.getElementById("paper");
+const scissorChoice = document.getElementById("scissor");
 let computerChoices;
 let showResult;
-let yourScore;
-let choiseString = (rockChoice + paperChoice + scissorChoice)
 
 // Make user choice and show computers choice and result
 rockChoice.addEventListener("click", function(){
-    yourChoice.innerHTML = "Rock"
-    generateComputerChoice()
-    showWinner()
-    matchWinner()
-})
+    yourChoice.innerHTML = "Rock";
+    generateComputerChoice();
+    showWinner();
+    matchWinner();
+});
 
 paperChoice.addEventListener("click", function(){
-    yourChoice.innerHTML = "Paper"
-    generateComputerChoice()
-    showWinner()
-    matchWinner()
-})
+    yourChoice.innerHTML = "Paper";
+    generateComputerChoice();
+    showWinner();
+    matchWinner();
+});
 
 scissorChoice.addEventListener("click", function(){
-    yourChoice.innerHTML = "Scissors"
-    generateComputerChoice()
-    showWinner()
-    matchWinner()
-})
+    yourChoice.innerHTML = "Scissors";
+    generateComputerChoice();
+    showWinner();
+    matchWinner();
+});
 
 
 // Generates random computer choice
 function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3) +1
+    const randomNumber = Math.floor(Math.random() * 3) +1;
 
     if (randomNumber === 1) {
-        computerChoices = "Paper"
+        computerChoices = "Paper";
     }
     if (randomNumber === 2) {
-        computerChoices = "Rock"
+        computerChoices = "Rock";
     }
     if (randomNumber === 3) {
-        computerChoices = "Scissors"
+        computerChoices = "Scissors";
     }
-    computerChoice.innerHTML = computerChoices
+    computerChoice.innerHTML = computerChoices;
 }
 
 // function to see if the user or computer won and increment scoreboard
 function showWinner() {
 
     if(computerChoices === yourChoice.innerHTML){
-        showResult = "It is a Tie!"
+        showResult = "It is a Tie!";
     }
     if(computerChoices === "Rock" && yourChoice.innerHTML === "Scissors"){
-        showResult = "You Lose!"
-        computerScoreIncrement()
+        showResult = "You Lose!";
+        computerScoreIncrement();
     }
     if(computerChoices === "Paper" && yourChoice.innerHTML === "Rock"){
-        showResult = "You Lose!"
-        computerScoreIncrement()
+        showResult = "You Lose!";
+        computerScoreIncrement();
     }
     if(computerChoices === "Scissors" && yourChoice.innerHTML === "Paper"){
-        showResult = "You Lose!"
-        computerScoreIncrement()
+        showResult = "You Lose!";
+        computerScoreIncrement();
     }
     if(computerChoices === "Rock" && yourChoice.innerHTML === "Paper"){
-        showResult = "You Win!"
-        userScoreIncrement()
+        showResult = "You Win!";
+        userScoreIncrement();
     }
     if(computerChoices === "Scissors" && yourChoice.innerHTML === "Rock"){
-        showResult = "You Win!"
-        userScoreIncrement()
+        showResult = "You Win!";
+        userScoreIncrement();
     }
     if(computerChoices === "Paper" && yourChoice.innerHTML === "Scissors"){
-        showResult = "You Win!"
-        userScoreIncrement()
+        showResult = "You Win!";
+        userScoreIncrement();
     }
     result.innerHTML = showResult;
 }
@@ -112,6 +108,4 @@ function matchWinner(){
     if(computerPoint === "3"){
         computerWins.style.display = "block";
     }
-    console.log(yourPoint);
-    console.log(computerPoint)
 }
